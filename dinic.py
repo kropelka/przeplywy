16da74ca814c75329d4470562ab9d7408027c0da
Karol Piotrowski <karolpiotrowski@onet.com.pl>
2014-06-25 04:10:52 +0200

    nowa lepsza jakosc

--- dinic.py
+++ dinic.py
@@ -9,123 +9,146 @@ import random
 import time
 import json
 
-class NetworkFlow:
+#step_by_step = False
+#do_next_step = False
+
+
+
+class NetworkFlow(object):
     def __init__(self):
         self.za = []
         self.wierzcholki = 0
         self.c = None
         self.przeplyw = None
         self.wartosc = 0
-    def wylosuj(self, n, m, maks_cap):
-        (self.za, self.c) = losowy_graf(n, m, maks_cap)
+        self.step_by_step = False
+        self.do_next_step = False
+        self.zorientowany = False
+    def wylosuj(self, n, m, maks_cap, orient=False):
+        (self.za, self.c) = self.losowy_graf(n, m, maks_cap, orient)
+        self.zorientowany = orient
         self.wierzcholki = n
     def maxflow(self, od, do):
-        (self.wartosc, self.przeplyw) = dinic(self.za, self.c, od, do)
+        self.wartosc = self.dinic(self.za, self.c, od, do)
+    def print_message(self, message):
+        print message
+    def losowy_graf(self, n, m, maks_cap, orient):
+        g = nx.gnm_random_graph(n, m)
+        graf = [ [] for x in range(0, n)]
+    #    print str(graf)
+        c = zeros((n, n))
+        for i in range(0, n):
+            for j in range(0,n):
+                if i < j: # gererujemy graf nieskierowany
+                    c[i,j] = random.randint(0, maks_cap)
+                    if orient:
+                        c[j,i] = c[i,j]
+                    else:
+                        c[j,i] = random.randint(0, maks_cap)
+        if orient:
+            self.zorientowany = True
+        #print str(g.edges())
+        for (u,v) in g.edges():
+    #        print 'Dodajemy wierzcholek'
+            (graf[u]).append(v)
+            graf[v].append(u)
+        #print str(graf)
+    #    for u in range(0,n):
+    #        for v in graf[u]:
+    #            print str(u) + ' -> '+ str(v)
+        return (graf, c)
 
 
 
+    def dinic(self, graf, c, s, t):
+        self.zorientowany = True
+        #global do_next_step
+        #global step_by_step
+        kolejka = deque()
+        calkowity_przeplyw = 0
+        n = len(graf)
+        faza = 0
+        przeplyw = zeros((n, n))
+        self.przeplyw = przeplyw
+        while True:
+            faza += 1
+            kolejka.appendleft(s)
+            dist = [-1]*n
+            dist[s] = 0
+            while kolejka:
+                u = kolejka.pop()
+                if self.step_by_step:
+                    self.print_message('Faza dzialania algorytmu Dinica nr '+str(faza))
+                    self.print_message('')
+                    self.print_message('BFS na wierzcholku '+str(u)+', dist['+str(u)+'] = '+str(dist[u]))
+                    self.print_message('Zawartosc kolejki: '+str(kolejka))
+                    while not self.do_next_step:
+                        time.sleep(0.001)
+                    self.do_next_step = False
+                for v in graf[u]:
+                    if dist[v]==-1 and c[u,v] > przeplyw[u,v]: # jezeli wierzcholek nie jest nasycony i nie zostal jeszcze odwiedzony, to dodaj go do mozliwej sciezki
+                        dist[v] = dist[u] + 1                  # blokujacej
+                        kolejka.appendleft(v)
+            if dist[t]==-1: # jezeli nie ma sciezki blokujacej od s do t, to zakoncz
+                self.print_message(u"Znaleziono maksymalny przepływ o wartości "+str(calkowity_przeplyw))
+                return calkowity_przeplyw
+            if self.step_by_step:
+                self.print_message(u"Podział sieci na warstwy:")
+                for i in range(0, dist[t]+1):
+                    tekst = "dist[v] = "+str(i) + " -> "
+                    for v in range(n):
+                        if dist[v]==i:
+                            tekst = tekst + str(v) + " "
+                    self.print_message(tekst)
 
-def losowy_graf(n, m, maks_cap):
-    g = nx.gnm_random_graph(n, m)
-    graf = [ [] for x in range(0, n)]
-    print str(graf)
-    c = zeros((n, n))
-    for i in range(0, n):
-        for j in range(0,n):
-            if i < j: # gererujemy graf nieskierowany
-                c[i,j] = random.randint(0, maks_cap)
-                c[j,i] = c[i,j]
-    print str(g.edges())
-    for (u,v) in g.edges():
-#        print 'Dodajemy wierzcholek'
-        (graf[u]).append(v)
-        graf[v].append(u)
-    print str(graf)
-#    for u in range(0,n):
-#        for v in graf[u]:
-#            print str(u) + ' -> '+ str(v)
-    return (graf, c)
+                self.print_message(" ")
+            ograniczenie = sum([c[s,v] for v in graf[s]])
+            calkowity_przeplyw += self.dinic_krok(graf, dist, c, przeplyw, s, t, ograniczenie) # przepychamy maksymalny mozliwy przeplyw ze zrodla przez sciezki blokujace
 
-step_by_step = False
-do_next_step = False
-
-
-def dinic(graf, c, s, t):
-    global do_next_step
-    global step_by_step
-    kolejka = deque()
-    calkowity_przeplyw = 0
-    n = len(graf)
-    faza = 0
-    przeplyw = zeros((n,n))
-    while True:
-        faza += 1
-        kolejka.appendleft(s)
-        dist = [-1]*n
-        dist[s] = 0
-        while kolejka:
-            u = kolejka.pop()
-            if step_by_step:
-                print 'Faza dzialania algorytmu Dinica nr '+str(faza)
-                print 'BFS na wierzcholku '+str(u)+', dist['+str(u)+'] = '+str(dist[u])
-                print 'Zawartosc kolejki: '+str(kolejka)
-                while not do_next_step:
-                    time.sleep(0.001)
-                do_next_step = False
-            for v in graf[u]:
-                if dist[v]==-1 and c[u,v] > przeplyw[u,v]: # jezeli wierzcholek nie jest nasycony i nie zostal jeszcze odwiedzony, to dodaj go do mozliwej sciezki
-                    dist[v] = dist[u] + 1                  # blokujacej
-                    kolejka.appendleft(v)
-        if dist[t]==-1: # jezeli nie ma sciezki blokujacej od s do t, to zakoncz
-            return (calkowity_przeplyw, przeplyw)
-        ograniczenie = sum([c[s,v] for v in graf[s]])
-        calkowity_przeplyw += dinic_krok(graf, dist, c, przeplyw, s, t, ograniczenie) # przepychamy maksymalny mozliwy przeplyw ze zrodla przez sciezki blokujace
-    print u"Znaleziono maksymalny przepływ o wartości "+str(calkowity_przeplyw)
-
-def dinic_krok(graf, dist, c, przeplyw, u, t, limit):
-    global step_by_step
-    global do_next_step
-    if limit <=0: # jak nie ma przeplywu do przepchniecia, to zakoncz
-        return 0
-    if u==t:  # jezeli dotarlismy do ujscia, to konczymy
-        return limit
-    val = 0
-    if step_by_step:
-        print 'Umieszczamy przeplyw '+str(limit)+' w sasiadach wierzcholka '+str(u)
-        while not do_next_step:
-            time.sleep(0.001)
-        do_next_step = False
-    for v in graf[u]:
-        res = c[u,v] - przeplyw[u,v]   # wyznaczamy wartosc rezydualna przeplywu na krawedzi (u,v)
-        if dist[v] == dist[u] + 1 and res>0:  # idziemy do nastepnej krawedzi na sciezce blokujacej
-            if step_by_step:
-                print 'Krawedz '+str(u)+'-'+str(v)+' na sciezce blokujacej'
-                print 'Wywoluje dinic_krok dla s = '+str(v)+' i ograniczenia = '+str(min(limit-val, res))
-                while not do_next_step:
-                    time.sleep(0.001)
-            av = dinic_krok(graf, dist, c, przeplyw, v, t, min(limit-val, res))  # rekurencyjnie umieszczamy tam reszte przeplywu
-            przeplyw[u,v] += av
-            przeplyw[v,u] -= av
-            val += av
-    if val==0:
-        dist[u] = -1
-    return val
+    def dinic_krok(self, graf, dist, c, przeplyw, u, t, limit):
+        #global step_by_step
+        #global do_next_step
+        if limit <=0: # jak nie ma przeplywu do przepchniecia, to zakoncz
+            return 0
+        if u==t:  # jezeli dotarlismy do ujscia, to konczymy
+            return limit
+        val = 0
+        if self.step_by_step:
+            self.print_message('Umieszczamy przeplyw '+str(limit)+' w sasiadach wierzcholka '+str(u))
+            while not self.do_next_step:
+                time.sleep(0.001)
+            do_next_step = False
+        for v in graf[u]:
+            res = c[u,v] - przeplyw[u,v]   # wyznaczamy wartosc rezydualna przeplywu na krawedzi (u,v)
+            if dist[v] == dist[u] + 1 and res>0:  # idziemy do nastepnej krawedzi na sciezce blokujacej
+                if self.step_by_step:
+                    self.print_message('Krawedz '+str(u)+'-'+str(v)+' na sciezce blokujacej')
+                    self.print_message('Wywoluje dinic_krok dla s = '+str(v)+' i ograniczenia = '+str(min(limit-val, res)))
+                    while not self.do_next_step:
+                        time.sleep(0.001)
+                av = self.dinic_krok(graf, dist, c, przeplyw, v, t, min(limit-val, res))  # rekurencyjnie umieszczamy tam reszte przeplywu
+                przeplyw[u,v] += av
+                przeplyw[v,u] -= av
+                val += av
+        if val==0:
+            dist[u] = -1
+        return val
 
 
-def randomizowana_proba_dinica(n, m, liczba_prob, maks_cap):
-    c = zeros((n, n))
-    calk_czas = 0
-    for i in range(1, liczba_prob):
-        (g,c) = losowy_graf(n, m, maks_cap)
-        timer = time.clock()
-        dinic(g, c, 0, n-1)
-        timer = time.clock() - timer
-        calk_czas += timer
-    return calk_czas/liczba_prob
+    def randomizowana_proba_dinica(self, n, m, liczba_prob, maks_cap, orient=False):
+        c = zeros((n, n))
+        calk_czas = 0
+        for i in range(1, liczba_prob):
+            (g,c) = self.losowy_graf(n, m, maks_cap, orient)
+            timer = time.clock()
+            self.dinic(g, c, 0, n-1)
+            timer = time.clock() - timer
+            calk_czas += timer
+        return calk_czas/liczba_prob
 
-def open_graph_file(file_name, g, c):
-    plik = open(file_name, 'r')
-    s = plik.readline()
-    liczba_wierzcholkow = int(s)
-    for line in plik:
-        print 'hello'
+    def open_graph_file(file_name, g, c):
+        plik = open(file_name, 'r')
+        s = plik.readline()
+        liczba_wierzcholkow = int(s)
+        for line in plik:
+            print 'hello'
--- dinic_gui.py
+++ dinic_gui.py
@@ -1,48 +1,84 @@
 # -*- coding: utf-8 -*-
-__author__ = 'karoru'
-import sys
-import math
 from gi.repository import Gtk
-Gtk.threads_init()
 import dinic
 import networkx as nx
 from matplotlib import use as mpluse
 mpluse('GTK3Agg')
-from matplotlib.figure import Figure
-from numpy import arange, sin, pi
 from matplotlib.backends.backend_gtk3agg import FigureCanvasGTK3Agg as FigureCanvas
 from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3 as NavigationToolbar
 import matplotlib.pyplot as plt
-import numpy
 import thread
-import glib
 from gi.repository import Gdk
 f = plt.figure()
 
+class ConsoleLikeTextBuffer:
+    def __init__(self):
+        self.textbuff = Gtk.TextBuffer()
+        self.end_iter = self.textbuff.get_end_iter()
+    def get_text_buffer(self):
+        return self.textbuff
+    def append_line(self, message):
+        self.textbuff.insert(self.end_iter, message + "\n")
+
+
 class ViewableNetworkFlow(dinic.NetworkFlow):
     layout = {}
+    dilayout = {}
+    def __init__(self):
+        super(ViewableNetworkFlow, self).__init__()
+        self.console = ConsoleLikeTextBuffer()
+    def print_message(self, message):
+        self.console.append_line(message)
+
     def show(self, reset_layout):
+        if not self.zorientowany:
+            plt.hold(False)
+            plt.tight_layout()
+            g = nx.Graph()
+            etykiety = {}
+            for u in range(0, self.wierzcholki):
+                g.add_nodes_from(range(self.wierzcholki))
+            for u in range(0,self.wierzcholki):
+                for v in self.za[u]:
+                    if self.przeplyw == None:
+                        etykiety[(u, v)] = str(self.c[u,v])
+                    else:
+                        etykiety[(u, v)] = str(self.przeplyw[u,v]) + "/" + str(self.przeplyw[v,u]) + "/" + str(self.c[u,v])
+                    g.add_edge(u, v)
+    #        print str(etykiety)
+            if (reset_layout==True or not(self.layout)):
+                self.layout = nx.spring_layout(g)
+            nx.draw(g, pos=self.layout)
+            nx.draw_networkx_edge_labels(g, pos=self.layout, edge_labels=etykiety)
+            f.canvas.draw()
+        else:
+            self.show_directed(reset_layout)
+    def show_directed(self, reset_layout):
+        format_etykiety = lambda u,v: str(self.przeplyw[u,v]) + "/" + str(self.c[u,v]) if not self.przeplyw == None else "0.0/"+str(self.c[u,v])
         plt.hold(False)
-        g = nx.Graph()
+        plt.tight_layout()
+        g = nx.DiGraph()
         etykiety = {}
         for u in range(0, self.wierzcholki):
             g.add_nodes_from(range(self.wierzcholki))
         for u in range(0,self.wierzcholki):
             for v in self.za[u]:
-                if self.przeplyw == None:
-                    etykiety[(u, v)] = str(self.c[u,v])
-                else:
-                    etykiety[(u, v)] = str(self.przeplyw[u,v]) + "/" + str(self.przeplyw[v,u]) + "/" + str(self.c[u,v])
-                g.add_edge(u, v)
-#        print str(etykiety)
-        if (reset_layout==True or not(self.layout)):
-            self.layout = nx.spring_layout(g)
-        nx.draw(g, pos=self.layout)
-        nx.draw_networkx_edge_labels(g, pos=self.layout, edge_labels=etykiety)
-        f.canvas.draw()
+                if ( self.c[u,v]>0 if self.przeplyw==None else  self.przeplyw[u, v] > 0):
+                    etykiety[(u,v)] = format_etykiety(u,v)
+                    g.add_edge(u,v)
+                elif ( self.c[v,u]>0  if self.przeplyw==None else self.przeplyw[v, u] > 0):
+                    etykiety[(v,u)] = format_etykiety(v,u)
+                    g.add_edge(v,u)
+        if (reset_layout==True or not (self.dilayout)):
+            self.dilayout = nx.spring_layout(g)
+        nx.draw(g, pos=self.dilayout)
+        nx.draw_networkx_edge_labels(g, pos=self.dilayout, edge_labels=etykiety)
+        f.canvas.draw()    
+
 
 class GraphTesting:
     def stat_show(self, t, s1, s2, title1, title2, titlex):
+        f.clf()
         ax = f.add_subplot(2, 1, 1)
         ax.set_title(title1)
         ax.set_xlabel(titlex)
@@ -53,17 +89,16 @@ class GraphTesting:
         ax.plot(t, s2)
         f.canvas.draw()
 
-    def const_v(self, n, maks_cap, tries):
+    def const_v(self, n, maks_cap, tries, orient):
         max_e = int(n*(n-1)/2)
         results = max_e * [0.0]
         results_by_t = int(n*(n-1)/2) * [0.0]
-        #t = numpy.arange(0, n, 1)
         t = [float(x) for x in range(0,int(n*(n-1)/2))]
         progress_bar = app.builder.get_object("test_progress")
         progress_bar.set_pulse_step(1.0/max_e)
         progress_bar.set_fraction(0.0)
         for j in range(3, int(n*(n-1)/2)):
-            calc_time = dinic.randomizowana_proba_dinica(n, j, tries, maks_cap)
+            calc_time = current_network.randomizowana_proba_dinica(n, j, tries, maks_cap, orient)
             results[j] = calc_time
             if j>3:
                 results[j] = results[j]/results[3]
@@ -74,7 +109,7 @@ class GraphTesting:
                 Gtk.main_iteration()
         self.stat_show(t, results, results_by_t, r'$T(|E|)$ przy $|V|=$const', r'$T(|E|)/|E|$', r'$|E|$')
         return results
-    def const_e(self, n, maks_cap, tries):
+    def const_e(self, n, maks_cap, tries, orient):
         ax = f.add_subplot(2, 1, 1)
         seq = []
         i = 1
@@ -90,14 +125,14 @@ class GraphTesting:
         progress_bar.set_pulse_step(1.0/(n-min_v))
         progress_bar.set_fraction(0.0)
         for j in range(min_v, n):
-            calc_time = dinic.randomizowana_proba_dinica(j, n, tries, maks_cap)
+            calc_time = current_network.randomizowana_proba_dinica(j, n, tries, maks_cap, orient)
             t.append(float(j))
             if j==min_v:
                 first_y = calc_time
                 results.append(calc_time)
             else:
                 results.append(calc_time/first_y)
-            results_by_t2.append(calc_time/(first_y*j))
+            results_by_t2.append(calc_time/(j))
             progress_bar.pulse()
             while Gtk.events_pending():
                 Gtk.main_iteration()
@@ -105,7 +140,7 @@ class GraphTesting:
 
 
 
-    def few_edges(self, n, maks_cap, tries):
+    def few_edges(self, n, maks_cap, tries, orient):
         progress_bar = app.builder.get_object("test_progress")
         progress_bar.set_pulse_step(1.0/n)
         progress_bar.set_fraction(0.0)
@@ -113,17 +148,17 @@ class GraphTesting:
         results = [0.0 for x in range(0, n)]
         results_by_t2 = [0.0 for x in range(0, n)]
         for j in range(3, n):
-            calc_time = dinic.randomizowana_proba_dinica(j, j, tries, maks_cap)
+            calc_time = current_network.randomizowana_proba_dinica(j, j, tries, maks_cap, orient)
             results[j] = calc_time
             if j>3:
                 results[j] = results[j]/results[3]
-            results_by_t2[j] = results[j]/(j*j)
+            results_by_t2[j] = results[j]/(j)
             progress_bar.pulse()
             while Gtk.events_pending():
                 Gtk.main_iteration()
-        self.stat_show(t, results, results_by_t2, r'$T(|V|)$ przy $|E| = O(|V|)$', r'$T(|V|)/|V|^2$', r'$|V|$')
+        self.stat_show(t, results, results_by_t2, r'$T(|V|)$ przy $|E| = O(|V|)$', r'$T(|V|)/|V|$', r'$|V|$')
 
-    def many_edges(self, n, maks_cap, tries):
+    def many_edges(self, n, maks_cap, tries, orient):
         progress_bar = app.builder.get_object("test_progress")
         progress_bar.set_pulse_step(1.0/n)
         progress_bar.set_fraction(0.0)
@@ -131,15 +166,15 @@ class GraphTesting:
         results = [0.0 for x in range(0, n)]
         results_by_t3 = [0.0 for x in range(0, n)]
         for j in range(3, n):
-            calc_time = dinic.randomizowana_proba_dinica(j, j*j/2, tries, maks_cap)
+            calc_time = current_network.randomizowana_proba_dinica(j, j*j/2, tries, maks_cap, orient)
             results[j] = calc_time
             if j>3:
                 results[j] = results[j]/results[3]
-            results_by_t3[j] = results[j]/(j*j*j)
+            results_by_t3[j] = results[j]/(j*j)
             progress_bar.pulse()
             while Gtk.events_pending():
                 Gtk.main_iteration()
-        self.stat_show(t, results, results_by_t3, r'$T(|V|)$ przy $|E| = \Omega(|V|^2)$', r'$T(|V|)/|V|^3$', r'$|V|$')
+        self.stat_show(t, results, results_by_t3, r'$T(|V|)$ przy $|E| = \Omega(|V|^2)$', r'$T(|V|)/|V|^2$', r'$|V|$')
 
 
 class UIHandler:
@@ -147,8 +182,7 @@ class UIHandler:
         box = Gtk.MessageDialog(message_type=Gtk.MessageType.QUESTION, flags = Gtk.DialogFlags.MODAL, buttons = Gtk.ButtonsType.YES_NO,
                                 message_format = u"Czy na pewno chcesz zakończyć pracę programu?", )
         response = box.run()
-        box.destroy()
-        print str(response)
+        box.hide()
         if response == Gtk.ResponseType.YES:
             Gtk.main_quit(*args)
 
@@ -156,8 +190,7 @@ class UIHandler:
         box = Gtk.MessageDialog(message_type=Gtk.MessageType.QUESTION, flags = Gtk.DialogFlags.MODAL, buttons = Gtk.ButtonsType.YES_NO,
                                 message_format = u"Czy na pewno chcesz zakończyć pracę programu?", )
         response = box.run()
-        box.destroy()
-        print str(response)
+        box.hide()
         if response == Gtk.ResponseType.YES:
             Gtk.main_quit()
 
@@ -170,43 +203,53 @@ class UIHandler:
         v = app.builder.get_object("vertices_box").get_value_as_int()
         e = app.builder.get_object("edges_box").get_value_as_int()
         max_cap = app.builder.get_object("maxcap_box").get_value_as_int()
-        print str(v)+' '+str(e)
+        oriented = app.builder.get_object("orientedcheckbox").get_active()
+        #print str(v)+' '+str(e)
         choice_dialog.hide()
         if response == 1:
-            current_network.wylosuj(v, e, max_cap)
+            current_network.wylosuj(v, e, max_cap, oriented)
 
     def onnextStepClicked(self, widget):
-        dinic.do_next_step = True
-        current_network.show(False)
+        current_network.do_next_step = True
+        current_network.show_directed(False)
 
     def onTestButtonClicked(self, button):
         mode_dialog = app.builder.get_object("testing_dialog")
         response = mode_dialog.run()
-        v = int(app.builder.get_object("test_vertices").get_text())
-        e = int(app.builder.get_object("test_edges").get_text())
+        mode_dialog.hide()
+        testtype_choice = app.builder.get_object("test_type").get_active()
+        if testtype_choice in {0, 2, 3}:
+            v = int(app.builder.get_object("test_vertices").get_text())
+        if testtype_choice==1:
+            e = int(app.builder.get_object("test_edges").get_text())
         tries = int(app.builder.get_object("no_tries").get_text())
         max_cap = int(app.builder.get_object("max_test_cap").get_text())
-        testtype_choice = app.builder.get_object("test_type").get_active()
+        orienttest = app.builder.get_object("orienttestcheckbox").get_active()
         if response == 1:
-            which_one = { 0: lambda: GraphTesting().const_v(v, max_cap, tries),
-                            1: lambda:GraphTesting().const_e(e, max_cap, tries),
-                            2: lambda:GraphTesting().few_edges(v, max_cap, tries),
-                            3: lambda:GraphTesting().many_edges(v, max_cap, tries)}
+            mode_dialog.show()
+            which_one = { 0: lambda: GraphTesting().const_v(v, max_cap, tries, orienttest),
+                            1: lambda:GraphTesting().const_e(e, max_cap, tries, orienttest),
+                            2: lambda:GraphTesting().few_edges(v, max_cap, tries, orienttest),
+                            3: lambda:GraphTesting().many_edges(v, max_cap, tries, orienttest)}
             which_one[testtype_choice]()
-        mode_dialog.hide()
+            mode_dialog.hide()
     def onToggleStepByStep(self, checkbox, data=None):
         if checkbox.get_active():
-            dinic.step_by_step = True
+            current_network.step_by_step = True
         else:
-            dinic.step_by_step = False
+            current_network.step_by_step = False
 
 
     def onMaxFlowButton(self, button):
-        source = int(app.builder.get_object("source_entry").get_text())
-        target = int(app.builder.get_object("target_entry").get_text())
-        if source == target:
+        try:
+            source = int(app.builder.get_object("source_entry").get_text())
+            target = int(app.builder.get_object("target_entry").get_text())
+            print str((source, target))
+            if source >= current_network.wierzcholki or target >= current_network.wierzcholki or source==target:
+                raise ValueError
+        except ValueError:
             box = Gtk.MessageDialog(message_type=Gtk.MessageType.WARNING, buttons = Gtk.ButtonsType.OK,
-                                    message_format = u"Przepływ nie może kończyć i zaczynać się w tym samym wierzchołku.")
+                                    message_format = u"Nieprawidłowe wartości pól źródło/ujście.")
             box.run()
             box.destroy()
         else:
@@ -223,14 +266,19 @@ class DinicGTK:
         self.builder.connect_signals(UIHandler())
         self.window = self.builder.get_object("main_window")
         self.window.connect("delete-event", UIHandler().onDeleteWindow)
+        xmax = Gdk.Screen.get_default().get_width()
+        ymax = Gdk.Screen.get_default().get_height()
         self.canvas = FigureCanvas(f)
-        self.canvas.set_size_request(Gdk.Screen.get_default().get_width()*0.7, Gdk.Screen.get_default().get_height()*0.7)
         self.builder.get_object("graphscreen").add_with_viewport(self.canvas)
+        self.builder.get_object("logscroll").set_size_request(int(0.3*xmax), int(0.65*ymax))
+        self.builder.get_object("graphscreen").set_size_request(int(0.4*xmax), int(0.69*ymax))
+        f.canvas.resize(int(0.35*xmax), int(0.68*ymax))
         toolbar = NavigationToolbar(self.canvas, self.window)
         self.builder.get_object("toolbar_box").pack_start(toolbar, False, False, 0)
-        self.textbuff = self.builder.get_object("algorithm_log")
-        self.window.show_all()
+        textview = self.builder.get_object("algorithm_log")
+        textview.set_buffer(current_network.console.get_text_buffer())
 
+        self.window.show_all()
 #        self.window = self.wTree.get_widget("main_window")
 #        if self.window:
 #            self.window.connect("destroy", gtk.main_quit)
